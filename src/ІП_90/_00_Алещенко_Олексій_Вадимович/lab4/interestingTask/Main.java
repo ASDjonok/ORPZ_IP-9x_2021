ae416fba1ed2eeec6f1816984fe5e8942f3b1efa
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-03-15 12:24:57 +0200

    add interesting task

---
+++ src/ІП_90/_00_Алещенко_Олексій_Вадимович/lab4/interestingTask/Main.java
@@ -0,0 +1,31 @@
+package ІП_90._00_Алещенко_Олексій_Вадимович.lab4.interestingTask;
+
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A(0);
+        System.out.println(a.fn.get());
+        a = a.map();
+        System.out.println(a.fn.get());
+    }
+
+    static class A {
+        int a;
+        public Supplier<Integer> fn = () -> a;
+
+        public A(int a) {
+            this.a = a;
+        }
+
+        public A(int a, Supplier<Integer> fn) {
+            this.a = a;
+            this.fn = fn;
+        }
+
+        public A map() {
+            return new A(a + 1, () -> a);
+        }
+    }
+}
+
